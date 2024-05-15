08c70eb1c3f35930155c37d513dcf467a7184264
alex-huth <alexander.huth@noaa.gov>
2024-05-15 16:27:50 -0400

    merged dev branches into all submodules. Added Mitch's diagnostic changes to SIS2.

--- ios_fms2_c5_nonsym_icepack.sh
+++ ios_fms2_c5_nonsym_icepack.sh
@@ -4,7 +4,5 @@ mkdir -p build/ice_oceanFMS2_SIS2_c5_nonsym_icepack/
 ../../src/mkmf/bin/list_paths -l ./ ../../src/Icepack/columnphysics ../../src/MOM6/config_src/{infra/FMS2,memory/dynamic_nonsymmetric,drivers/FMS_cap,external} ../../src/MOM6/src/{*,*/*}/ ../../src/SIS2/config_src/dynamic_symmetric ../../src/{atmos_null,SIS2/src,SIS2,coupler/full,coupler/shared,coupler,land_null,ice_param,icebergs/src,FMS2/coupler,FMS2/include,FMS2})
 (cd build/ice_oceanFMS2_SIS2_c5_nonsym_icepack/; \
 ../../src/mkmf/bin/mkmf -t ../../src/mkmf/templates/ncrc-intel.mk -o '-I../fms2' -p MOM6_SIS2_Alex_bergs -l '-L../fms2 -lfms' -c '-Duse_AM3_physics -D_USE_LEGACY_LAND_ -DUSE_FMS2_IO -Duse_libMPI -Duse_netCDF' path_names )
-cd build/ice_oceanFMS2_SIS2_c5_nonsym_icepack/
-source ../env_c5
-make NETCDF=3 REPRO=1 MOM6_SIS2_Alex_bergs -j
-cd ../..
+source ./build/env_c5
+make -C ./build/ice_oceanFMS2_SIS2_c5_nonsym_icepack NETCDF=3 REPRO=1 MOM6_SIS2_Alex_bergs -j
