6bb5536595e7516116996a71e169d45d92080b07
alex-huth <alexander.huth@noaa.gov>
2024-10-18 18:20:47 -0400

    Weekly submodule merges with main and dev/gfdl. Some udates for compiling with the new c5 environment

--- fms2_build.sh
+++ fms2_build.sh
@@ -2,9 +2,12 @@
 mkdir -p build/fms2/
 (cd build/fms2; rm -f path_names; \
 ../../src/mkmf/bin/list_paths -l ../../src/FMS2; \
-../../src/mkmf/bin/mkmf -t ../../src/mkmf/templates/ncrc-intel.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF" path_names)
+../../src/mkmf/bin/mkmf -t ../../src/mkmf/templates/ncrc5-intel.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF" path_names)
 
-cd build/fms2/
-source ../env_c5
-make NETCDF=3 REPRO=1 libfms.a -j
-cd ../..
+# cd build/fms2/
+# source ../env_c5
+# make NETCDF=3 REPRO=1 libfms.a -j
+# cd ../..
+
+source ./build/env_c5
+make -C ./build/fms2 NETCDF=3 REPRO=1 libfms.a -j
