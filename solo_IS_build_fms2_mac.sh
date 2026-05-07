5bf9e12044ade49fde24df17ab00fd1507563d54
Alexander Huth <alexander.huth@noaa.gov>
2026-05-07 10:13:16 -0400

    Regular MPM updates

--- solo_IS_build_fms2_mac.sh
+++ solo_IS_build_fms2_mac.sh
@@ -4,4 +4,4 @@ mkdir -p build/IS_only_fms2_mac/
 ../../src/mkmf/bin/list_paths -l ./ ../../src/MOM6/{config_src/infra/FMS2,config_src/memory/dynamic_symmetric,config_src/drivers/ice_solo_driver,config_src/external,src/{*,*/*}}/ ; \
  ../../src/mkmf/bin/mkmf -t ../../src/mkmf/templates/osx-gcc10.mk -o '-I../fms2' -p MOM6 -l '-L../fms2 -lfms' -c '-Duse_libMPI -Duse_netCDF' path_names)
 #source ./build/env_c5
-make -C ./build/IS_only_fms2_mac NETCDF=3 REPRO=1 MOM6 -j
+make -C ./build/IS_only_fms2_mac NETCDF=3 REPRO=1 MOM6 -j 8
